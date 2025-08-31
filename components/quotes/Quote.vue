21200e8b52e5e1897e58e4c104fcb5621b6b06d8
vitae322 <125974058+kakashaUbivasha@users.noreply.github.com>
2025-08-31 16:19:59 +0500

    fix: make quote section responsive

--- components/quotes/Quote.vue
+++ components/quotes/Quote.vue
@@ -1,46 +1,15 @@
 <script setup lang="ts">
 defineProps({
-  title: '',
-  author: '',
-  text: ''
+  title: { type: String, default: '' },
+  author: { type: String, default: '' },
+  text: { type: String, default: '' }
 })
 </script>
 
 <template>
-<div class="item">
-  <h2 class="text-1xl">{{title}}</h2>
-  <p>{{text}}</p>
-  <span>{{author}}</span>
-</div>
+  <div class="flex flex-col justify-between p-5 h-40 w-full max-w-md mx-auto rounded-2xl border border-gray-200 shadow">
+    <h2 class="text-lg font-bold text-center">{{ title }}</h2>
+    <p class="text-base text-center">{{ text }}</p>
+    <span class="italic text-sm text-right">{{ author }}</span>
+  </div>
 </template>
-
-<style scoped>
-.item{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border: 1px solid #eaeaea;
-  padding: 20px;
-  height: 150px;
-  border-radius: 20px;
-  width: 800px;
-  box-shadow:
-      0 4px 8px rgba(0, 0, 0, 0.1);  /* Мягкая тень снизу */
-      /* Глубокая тень для объёмности */
-
-  h2{
-    font-size: 18px !important;
-    font-weight: 700;
-    text-align: center;
-  }
-  p{
-    font-size: 16px !important;
-    text-align: center;
-  }
-  span{
-    font-style: italic;
-    font-size: 12px;
-    text-align: end;
-  }
-}
-</style>

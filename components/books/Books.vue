132b0274fee36666b5f3462a354e2edef7b0bb04
vitae322 <125974058+kakashaUbivasha@users.noreply.github.com>
2025-10-05 22:51:47 +0500

    Expand book cards with additional metadata

--- components/books/Books.vue
+++ components/books/Books.vue
@@ -11,6 +11,27 @@ const perPage = 12;
 
 const { fetchRandomBooks } = useBooks();
 
+const resolveGenre = (book) =>
+  book?.genre?.name ||
+  book?.genre?.title ||
+  book?.genre_name ||
+  book?.genre;
+
+const resolvePublicationDate = (book) =>
+  book?.publication_date ||
+  book?.publicationDate ||
+  book?.published_at ||
+  book?.publishedAt ||
+  book?.release_date ||
+  book?.releaseDate;
+
+const resolveDescription = (book) =>
+  book?.description ||
+  book?.short_description ||
+  book?.shortDescription ||
+  book?.summary ||
+  book?.annotation;
+
 const loadInitialBooks = async () => {
   isInitialLoading.value = true;
   try {
@@ -68,6 +89,9 @@ onMounted(() => {
               :title="book.title"
               :author="book.author"
               :image-src="book.image"
+              :genre="resolveGenre(book)"
+              :publication-date="resolvePublicationDate(book)"
+              :description="resolveDescription(book)"
               :id ="book.id"
           />
         </div>
